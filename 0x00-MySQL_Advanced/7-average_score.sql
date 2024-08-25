9314dc8cf4286a70347c4acc7ac73d23c624138e
Ustaz07 <shamsutukor@gmail.com>
2024-08-24 17:47:00 -0700

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,11 @@
--- Set the delimiter for creating the procedure
+-- An SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
 DELIMITER $$
-
--- Create the stored procedure
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-
-    -- Compute the average score for the given user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the user's average_score with the computed value
-    UPDATE users
-    SET average_score = COALESCE(avg_score, 0)
-    WHERE id = user_id;
-END$$
-
--- Reset the delimiter to default
-DELIMITER ;
+	UPDATE users
+		SET average_score = (SELECT AVG(score)
+					FROM corrections
+					WHERE corrections.user_id = user_id)
+		WHERE id = user_id;
+END $$
